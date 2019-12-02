a926ca9d58540c42633629d1114e31c66ee8f8ad
Ankur Bansal <ankur.bansal2@publicissapient.com>
2019-12-02 16:57:10 +0530

    Map view resize pin view image

--- MapApplication/MapApplication/Views/VehicleMapViewController.swift
+++ MapApplication/MapApplication/Views/VehicleMapViewController.swift
@@ -52,7 +52,12 @@ extension VehicleMapViewController : MKMapViewDelegate{
         }
         
         if let cachedImage = AppDelegate.imageCache.object(forKey: annotation.carImageUrl as NSString) {
-            view.image = cachedImage
+            // Resize image
+            let size = CGSize(width: cachedImage.size.width/2, height: cachedImage.size.height/2)
+            UIGraphicsBeginImageContext(size)
+            cachedImage.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+            let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+            view.image = resizedImage
            
             
         }
